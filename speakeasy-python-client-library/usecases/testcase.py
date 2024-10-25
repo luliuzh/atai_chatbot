ecc12e88ebe94292dd01855cbb627974a48d6a6e
Lu Li <stull2023@163.com>
2024-10-25 23:03:04 +0200

    examples for use

--- speakeasy-python-client-library/usecases/testcase.py
+++ speakeasy-python-client-library/usecases/testcase.py
@@ -1,6 +1,7 @@
 from factual_q import Query_Processer
+from graph_kg import graph
 
-my_query_processer = Query_Processer()
+my_query_processer = Query_Processer(graph)
 queries = [
     'Who is the director of Good Will Hunting? ',
     'Who directed The Bridge on the River Kwai? ',
@@ -12,33 +13,18 @@ queries = [
 ]
 for query in queries:
     print(f'query:{query}')
+    # entity = my_query_processer.entity_extractor(query)
     entity = my_query_processer.entity_extractor(query)
     relation = my_query_processer.relation_extractor(query)
     print(f"entity:{entity}")
     print(f"ralation:{relation}'\n")
-# sample use
-# query = 'Who is the director of Star Wars: Episode VI - Return of the Jedi? '
-# entity = my_query_processer.entity_extractor(query)
-# relation = my_query_processer.relation_extractor(query)
-# print(entity)
-# print(relation)
 
-# query = 'Who is the screenwriter of The Masked Gang: Cyprus? '
-# entity = my_query_processer.entity_extractor(query)
-# relation = my_query_processer.relation_extractor(query)
-# print(entity)
-# print(relation)
+# entity: list, relation: dict
+for entity_item, relation_item in zip(entity, relation.values()):
+    entity_value = list(entity_item.values())[0]
+    relation_value = relation_item
+    result = my_query_processer.query_knowledge_graph(entity=entity_value, relation=relation_value)
+    print(result)
 
-# query = "What is the MPAA film rating of Weathering with You? "
-# entity = my_query_processer.entity_extractor(query)
-# relation = my_query_processer.relation_extractor(query)
-# print(entity)
-# print(relation)
-
-# query = "What is the genre of Good Neighbors? "
-# entity = my_query_processer.entity_extractor(query)
-# relation = my_query_processer.relation_extractor(query)
-# print(entity)
-# print(relation)
-
+    
 
